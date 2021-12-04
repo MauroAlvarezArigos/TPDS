69fd547ea99c136560ac6d5b8c82672643f87f96
Matias Ifran <ifran2855@gmail.com>
2021-12-04 10:26:49 -0300

    limpiando codigo

--- src/DAO/utils/DB.java
+++ src/DAO/utils/DB.java
@@ -12,9 +12,9 @@ public class DB {
 
     private static Connection crearConexion() {
 
-        String url =  "jdbc:postgresql://localhost:5432/TPDisenio";
+        String url =  "jdbc:postgresql://localhost:5432/disenioSistemasTP";
         String user = "postgres";
-        String pass = "0000";
+        String pass = "000";
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url, user, pass);
