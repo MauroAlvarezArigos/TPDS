26635bc90acae5159f8401a9501f2aa64af4a916
Matias Ifran <ifran2855@gmail.com>
2021-12-09 20:28:47 -0300

    acomodando tablas

--- database/Database.sql
+++ database/Database.sql
@@ -4,11 +4,18 @@ create table posIVA (
 	constraint pk_ident primary key (ident)
 );
 
-create table pais(
-	nombre character varying(50),
-	codigoPais int,
-	nacionalidad character varying(50),
-	constraint pk_codigoPais primary key (codigoPais)
+create table unidades (
+	id_unidades INT,
+	fechaconsumo DATE,
+	unidades INT,
+	constraint pk_idunidades primary key (id_unidades)
+);
+
+create table pais (
+	id_pais INT,
+	nombre VARCHAR(50),
+	nacionalidad VARCHAR(50),
+	constraint pk_idpais primary key (id_pais)
 );
 
 create table provincia(
@@ -16,7 +23,7 @@ create table provincia(
 	codigoProvincia int,
 	pais int,
 	constraint pk_codigoProv primary key (codigoProvincia),
-	constraint fk_pais foreign key (pais) REFERENCES pais (codigoPais)
+	constraint fk_pais foreign key (pais) REFERENCES pais (id_pais)
 );
 
 create table localidad(
@@ -26,6 +33,42 @@ create table localidad(
 	prov int,
 	constraint pk_codigoLocalidad primary key (codigoLocalidad),
 	constraint fk_prov foreign key (prov) REFERENCES provincia (codigoProvincia)
+);
+
+create table IDType(
+	tipoDeID character varying(10),
+	constraint pk_Tipo primary key (tipoDeID)
+);
+
+create table seccionconsumo (
+	seccion varchar(50),
+	constraint pk_seccion primary key (seccion)
+);
+
+create table tipohabitacion (
+    id int,
+    tipo character varying(50),
+    costo double precision,
+    constraint pk_id primary key (id)
+);
+
+create table itemconsumo (
+	id_item INT,
+	categoria VARCHAR(50),
+	nombre VARCHAR(50),
+	costo VARCHAR(50),
+	constraint pk_iditem primary key (id_item),
+	constraint fk_categoria foreign key (categoria) references seccionconsumo(seccion)
+);
+
+create table habitacion (
+    numero int,
+    piso int,
+    tipo int,
+    capacidad int,
+    descuento int,
+    constraint pk_habitacion primary key (numero, piso),
+    constraint fk_tipo foreign key (tipo) REFERENCES tipohabitacion (id)
 );
 
 create table persona(
@@ -41,11 +84,6 @@ create table persona(
 	constraint fk_Localidad foreign key (Localidad) REFERENCES localidad (codigoLocalidad)
 );
 
-create table IDType(
-	tipoDeID character varying(10),
-	constraint pk_Tipo primary key (tipoDeID)
-);
-
 create table pasajero(
     idPersona int,
 	nombre character varying(50),
@@ -57,26 +95,8 @@ create table pasajero(
 	Nacionalidad int,
 	constraint fk_idpersona foreign key (idPersona) REFERENCES persona (idPersona),
 	constraint pk_idPersona primary key (idPersona),
-	constraint fk_nacionalidad foreign key (Nacionalidad) REFERENCES pais (codigoPais),
+	constraint fk_nacionalidad foreign key (Nacionalidad) REFERENCES pais (id_pais),
 	constraint fk_TipoDoc foreign key (TipoDoc) REFERENCES IDtype (tipoDeID)
-);
-
-
-create table tipohabitacion (
-    id int,
-    tipo character varying(50),
-    costo double precision,
-    constraint pk_id primary key (id)
-);
-
-create table habitacion (
-    numero int,
-    piso int,
-    tipo int,
-    capacidad int,
-    descuento int,
-    constraint pk_habitacion primary key (numero, piso),
-    constraint fk_tipo foreign key (tipo) REFERENCES tipohabitacion (id)
 );
 
 create table ocupacion (
@@ -89,6 +109,34 @@ create table ocupacion (
     constraint pk_id_ocupacion primary key (id_ocupacion),
     constraint fk_habitacion foreign key (numero, piso) REFERENCES habitacion (numero, piso),
     constraint fk_responsable foreign key (responsable) REFERENCES pasajero (idPersona)
+);
+
+create table consumoservicios (
+	id_consumo INT,
+	ocupacion int,
+	costototal VARCHAR(50),
+	constraint pk_idconsumo primary key (id_consumo),
+	constraint fk_ocupacion foreign key (ocupacion) references ocupacion (id_ocupacion)
+);
+
+create table consumounidades (
+	id_unidades INT,
+	id_consumo INT,
+	id_item INT,
+	constraint fk_idunidades foreign key (id_unidades) references unidades(id_unidades),
+	constraint fk_idconsumo foreign key (id_consumo) references consumoservicios(id_consumo),
+	constraint fk_iditem foreign key (id_item) references itemconsumo(id_item),
+	constraint pf_consumo_unidades primary key (id_unidades, id_consumo, id_item)
+);
+
+create table periodoestadia (
+	id_estadia INT,
+	ocupacion int,
+	fechainicio DATE,
+	fechafinal DATE,
+	monto VARCHAR(50),
+	constraint pk_idestadia primary key (id_estadia),
+	constraint fk_ocupacion foreign key (ocupacion) references ocupacion (id_ocupacion)
 );
 
 create table reserva (
--- database/NewDatosTablas.sql
+++ database/NewDatosTablas.sql
@@ -17,29 +17,41 @@ insert into posiva (ident, tipo) values (1, 'consumidor final');
 insert into posiva (ident, tipo) values (2, 'monotributista');
 insert into posiva (ident, tipo) values (3, 'responsable inscripto');
 
+--unidades
+insert into unidades (id_unidades, fechaconsumo, unidades) values (019, '21/03/2021', 15);
+insert into unidades (id_unidades, fechaconsumo, unidades) values (029, '21/10/2021', 22);
+insert into unidades (id_unidades, fechaconsumo, unidades) values (039, '12/11/2020', 30);
+insert into unidades (id_unidades, fechaconsumo, unidades) values (049, '02/08/2021', 4);
+insert into unidades (id_unidades, fechaconsumo, unidades) values (059, '26/01/2021', 5);
+insert into unidades (id_unidades, fechaconsumo, unidades) values (069, '01/03/2021', 6);
+insert into unidades (id_unidades, fechaconsumo, unidades) values (079, '04/11/2021', 7);
+insert into unidades (id_unidades, fechaconsumo, unidades) values (089, '14/10/2021', 8);
+insert into unidades (id_unidades, fechaconsumo, unidades) values (099, '20/07/2021', 9);
+insert into unidades (id_unidades, fechaconsumo, unidades) values (0119, '16/03/2021', 10);
+
 --Datos Pais
-insert into pais (nombre, codigoPais, nacionalidad) values ('Chile', 1, 'Chileno');
-insert into pais (nombre, codigoPais, nacionalidad) values ('Ukraine', 2, 'Uckraniano');
-insert into pais (nombre, codigoPais, nacionalidad) values ('Rusia', 3, 'Ruso');
-insert into pais (nombre, codigoPais, nacionalidad) values ('Brazil', 4, 'Brasilero');
-insert into pais (nombre, codigoPais, nacionalidad) values ('France', 5, 'Frances');
-insert into pais (nombre, codigoPais, nacionalidad) values ('Turquia', 6, 'Turco');
-insert into pais (nombre, codigoPais, nacionalidad) values ('Argentina', 7, 'Argentino');
-insert into pais (nombre, codigoPais, nacionalidad) values ('United States', 8, 'Americano');
-insert into pais (nombre, codigoPais, nacionalidad) values ('Philippines', 9, 'Filipino');
-insert into pais (nombre, codigoPais, nacionalidad) values ('Japon', 10, 'Japones');
+insert into pais (id_pais, nombre, nacionalidad) values (054, 'Argentina', 'Argentina');
+insert into pais (id_pais, nombre, nacionalidad) values (055, 'Colombia', 'Colombia');
+insert into pais (id_pais, nombre, nacionalidad) values (056, 'Vietnam', 'Vietnam');
+insert into pais (id_pais, nombre, nacionalidad) values (057, 'Ecuador', 'Ecuador');
+insert into pais (id_pais, nombre, nacionalidad) values (058, 'Brasil', 'Brasil');
+insert into pais (id_pais, nombre, nacionalidad) values (059, 'Chile', 'Chile');
+insert into pais (id_pais, nombre, nacionalidad) values (060, 'Uruguay', 'Uruguay');
+insert into pais (id_pais, nombre, nacionalidad) values (061, 'Cuba', 'Cuba');
+insert into pais (id_pais, nombre, nacionalidad) values (062, 'Estados Unidos', 'Estados Unidos');
+insert into pais (id_pais, nombre, nacionalidad) values (063, 'Mexico', 'Mexico');
 
 --Datos Provincia
-insert into provincia (nombre, codigoProvincia, Pais) values ('Santa fe', 1, '7');
-insert into provincia (nombre, codigoProvincia, Pais) values ('Entre Rios', 2, '7');
-insert into provincia (nombre, codigoProvincia, Pais) values ('Texas', 3, '8');
-insert into provincia (nombre, codigoProvincia, Pais) values ('Florida', 4, '8');
-insert into provincia (nombre, codigoProvincia, Pais) values ('Sao Paulo', 5, '4');
-insert into provincia (nombre, codigoProvincia, Pais) values ('Bahia', 6, '4');
-insert into provincia (nombre, codigoProvincia, Pais) values ('Moscu', 7, '3');
-insert into provincia (nombre, codigoProvincia, Pais) values ('Antofagasta', 8, '1');
-insert into provincia (nombre, codigoProvincia, Pais) values ('Valpara�so', 9, '1');
-insert into provincia (nombre, codigoProvincia, Pais) values ('Normand�a', 10, '5');
+insert into provincia (nombre, codigoProvincia, Pais) values ('Santa fe', 1, 054);
+insert into provincia (nombre, codigoProvincia, Pais) values ('Entre Rios', 2, 054);
+insert into provincia (nombre, codigoProvincia, Pais) values ('Texas', 3, 062);
+insert into provincia (nombre, codigoProvincia, Pais) values ('Florida', 4, 062);
+insert into provincia (nombre, codigoProvincia, Pais) values ('Sao Paulo', 5, 058);
+insert into provincia (nombre, codigoProvincia, Pais) values ('Bahia', 6, 058);
+insert into provincia (nombre, codigoProvincia, Pais) values ('Ciudad de Mexico', 7, 063);
+insert into provincia (nombre, codigoProvincia, Pais) values ('Antofagasta', 8, 059);
+insert into provincia (nombre, codigoProvincia, Pais) values ('Valparaiso', 9, 059);
+insert into provincia (nombre, codigoProvincia, Pais) values ('Maldonado', 10, 060);
 
 --Datos Localidad
 insert into localidad (nombre, codPostal, codigoLocalidad, prov) values ('Santa fe ciudad', '3000', 1, '1');
@@ -51,7 +63,7 @@ insert into localidad (nombre, codPostal, codigoLocalidad, prov) values ('Housto
 insert into localidad (nombre, codPostal, codigoLocalidad, prov) values ('Moscu', '586', 7, '7');
 insert into localidad (nombre, codPostal, codigoLocalidad, prov) values ('Rouen', '123', 8, '5');
 insert into localidad (nombre, codPostal, codigoLocalidad, prov) values ('Castilho', '3075', 9, '5');
-insert into localidad (nombre, codPostal, codigoLocalidad, prov) values ('Valpara�so', '9345', 10, '9');
+insert into localidad (nombre, codPostal, codigoLocalidad, prov) values ('Valparaiso', '9345', 10, '9');
 
 --Datos IDType
 insert into IDType (tipoDeID) values ('DNI');
@@ -59,6 +71,79 @@ insert into IDType (tipoDeID) values ('LC');
 insert into IDType (tipoDeID) values ('Otro');
 insert into IDType (tipoDeID) values ('LE');
 insert into IDType (tipoDeID) values ('Pasaporte');
+
+--Seccion consumo
+insert into seccionconsumo (seccion) values ('Higene');
+insert into seccionconsumo (seccion) values ('Bebida sin alcohol');
+insert into seccionconsumo (seccion) values ('Belleza');
+insert into seccionconsumo (seccion) values ('Comida');
+insert into seccionconsumo (seccion) values ('Farmacia');
+insert into seccionconsumo (seccion) values ('Bebida con alcohol');
+
+--Datos TipoHabitacion
+insert into tipohabitacion (id, tipo, costo) values (1, 'Simple', 99.99);
+insert into tipohabitacion (id, tipo, costo) values (2, 'Doble', 199.99);
+
+--Item consumo
+insert into itemconsumo (id_item, categoria, nombre, costo) values (0199, 'Higene', 'PapelHigenico', '$52.95');
+insert into itemconsumo (id_item, categoria, nombre, costo) values (0299, 'Bebida sin alcohol', 'Sprite', '$7.57');
+insert into itemconsumo (id_item, categoria, nombre, costo) values (0399, 'Belleza', 'Sombra para Ojos', '$92.40');
+insert into itemconsumo (id_item, categoria, nombre, costo) values (0499, 'Bebida sin alcohol', 'Cocacola', '$8.36');
+insert into itemconsumo (id_item, categoria, nombre, costo) values (0599, 'Comida', 'Hamburguesa con papas', '$77.88');
+insert into itemconsumo (id_item, categoria, nombre, costo) values (0699, 'Comida', 'Hamburguesa con papas', '$29.36');
+insert into itemconsumo (id_item, categoria, nombre, costo) values (0799, 'Higene', 'Desodorante', '$91.67');
+insert into itemconsumo (id_item, categoria, nombre, costo) values (0899, 'Farmacia', 'Ibuprofeno', '$61.10');
+insert into itemconsumo (id_item, categoria, nombre, costo) values (0999, 'Farmacia', 'Paracetamol', '$52.54');
+insert into itemconsumo (id_item, categoria, nombre, costo) values (01099, 'Bebida con alcohol', 'Vino Blanco', '$35.41');
+
+--Datos Habitacion
+insert into habitacion (numero, piso, tipo, capacidad, descuento) values ( 1, 1, 1, 1, 0);
+insert into habitacion (numero, piso, tipo, capacidad, descuento) values ( 2, 1, 2, 2, 0);
+insert into habitacion (numero, piso, tipo, capacidad, descuento) values ( 3, 1, 1, 1, 0);
+insert into habitacion (numero, piso, tipo, capacidad, descuento) values ( 1, 2, 1, 1, 0);
+insert into habitacion (numero, piso, tipo, capacidad, descuento) values ( 2, 2, 2, 2, 0);
+insert into habitacion (numero, piso, tipo, capacidad, descuento) values ( 3, 2, 1, 2, 0);
+
+--Datos Ocupacion
+insert into ocupacion (id_ocupacion, numero, piso, responsable, checkin, checkout) values (1, 1, 2, id1, '2021-09-21', '2021-09-30');
+insert into ocupacion (id_ocupacion, numero, piso, responsable, checkin, checkout) values (2, 1, 2, id2, '2021-09-01', '2021-09-12');
+insert into ocupacion (id_ocupacion, numero, piso, responsable, checkin, checkout) values (3, 1, 2, id3, '2021-09-13', '2021-09-15');
+
+--Periodo estadia
+insert into periodoestadia (id_estadia, ocupacion, fechainicio, fechafinal, monto) values (00001, 1, '25/11/2021', '21/05/2023', '$600.86');
+insert into periodoestadia (id_estadia, ocupacion, fechainicio, fechafinal, monto) values (00002, 2, '12/01/2021', '22/02/2023', '$6758.76');
+insert into periodoestadia (id_estadia, ocupacion, fechainicio, fechafinal, monto) values (00003, 3, '23/03/2021', '23/08/2023', '$8523.47');
+insert into periodoestadia (id_estadia, ocupacion, fechainicio, fechafinal, monto) values (00004, 2, '07/09/2021', '06/06/2023', '$2145.57');
+insert into periodoestadia (id_estadia, ocupacion, fechainicio, fechafinal, monto) values (00005, 3, '24/07/2021', '16/05/2023', '$7609.79');
+insert into periodoestadia (id_estadia, ocupacion, fechainicio, fechafinal, monto) values (00006, 1, '21/02/2021', '03/06/2023', '$5541.41');
+insert into periodoestadia (id_estadia, ocupacion, fechainicio, fechafinal, monto) values (00007, 3, '14/09/2021', '19/03/2022', '$9025.69');
+insert into periodoestadia (id_estadia, ocupacion, fechainicio, fechafinal, monto) values (00008, 2, '20/10/2021', '31/01/2023', '$6675.94');
+insert into periodoestadia (id_estadia, ocupacion, fechainicio, fechafinal, monto) values (00009, 1, '04/01/2021', '06/01/2022', '$4943.51');
+insert into periodoestadia (id_estadia, ocupacion, fechainicio, fechafinal, monto) values (000010,3, '16/04/2021', '18/08/2023', '$1221.74');
+
+--Consumo servicio
+insert into consumoservicios (id_consumo, ocupacion, costototal) values (01, 1, '$3724.02');
+insert into consumoservicios (id_consumo, ocupacion, costototal) values (02, 2, '$1216.90');
+insert into consumoservicios (id_consumo, ocupacion, costototal) values (03, 1, '$4066.48');
+insert into consumoservicios (id_consumo, ocupacion, costototal) values (04, 3, '$443.56');
+insert into consumoservicios (id_consumo, ocupacion, costototal) values (05, 3, '$4937.15');
+insert into consumoservicios (id_consumo, ocupacion, costototal) values (06, 3, '$4785.82');
+insert into consumoservicios (id_consumo, ocupacion, costototal) values (07, 1, '$5041.43');
+insert into consumoservicios (id_consumo, ocupacion, costototal) values (08, 2, '$8699.82');
+insert into consumoservicios (id_consumo, ocupacion, costototal) values (09, 2, '$9159.50');
+insert into consumoservicios (id_consumo, ocupacion, costototal) values (011, 2, '$9969.75');
+
+--Consumo unidades
+insert into consumounidades (id_unidades, id_consumo, id_item) values (019, 01, 0199);
+insert into consumounidades (id_unidades, id_consumo, id_item) values (029, 02, 0299);
+insert into consumounidades (id_unidades, id_consumo, id_item) values (039, 03, 0399);
+insert into consumounidades (id_unidades, id_consumo, id_item) values (049, 04, 0499);
+insert into consumounidades (id_unidades, id_consumo, id_item) values (059, 05, 0599);
+insert into consumounidades (id_unidades, id_consumo, id_item) values (069, 06, 0699);
+insert into consumounidades (id_unidades, id_consumo, id_item) values (079, 07, 0799);
+insert into consumounidades (id_unidades, id_consumo, id_item) values (089,08, 0899);
+insert into consumounidades (id_unidades, id_consumo, id_item) values (099, 09, 0999);
+insert into consumounidades (id_unidades, id_consumo, id_item) values (0119, 011, 01099);
 
 --Datos persona
 insert into persona ( telefono, email, cuit, calle, altura, posIVA, localidad) values ( '3507913321', 'dglasbey0@whitehouse.gov', '20496352893', 'Veith', '78179', 1, 4) returning idpersona into id1;
@@ -73,38 +158,21 @@ insert into persona ( telefono, email, cuit, calle, altura, posIVA, localidad) v
 insert into persona ( telefono, email, cuit, calle, altura, posIVA, localidad) values ( '9630048949', 'lthuillier9@wisc.edu', '6325964203', 'Sherman', '1129', 3, 8)returning idpersona into id10;
 
 --Datos Pasajero
-insert into pasajero (idpersona, nombre, apellido, Ndoc, TipoDoc, Ocupacion, FechaNac, nacionalidad) values (id1, 'Bondy', 'Rylatt', '3808721502', 'LC', 'Office Assistant IV', '2021-09-11', 1);
-insert into pasajero (idpersona, nombre, apellido, Ndoc, TipoDoc, Ocupacion, FechaNac, nacionalidad) values (id2, 'Luce', 'Theml', '9936502027', 'LE', 'Chief Design Engineer', '2021-02-13', 1);
-insert into pasajero (idpersona, nombre, apellido, Ndoc, TipoDoc, Ocupacion, FechaNac, nacionalidad) values (id3, 'Ricki', 'Lapworth', '7519091015', 'Otro', 'Food Chemist', '2021-06-21', 4);
-insert into pasajero (idpersona, nombre, apellido, Ndoc, TipoDoc, Ocupacion, FechaNac, nacionalidad) values (id4, 'Franny', 'Gumme', '9093832379', 'LC', 'Junior Executive', '2021-08-10', 5);
-insert into pasajero (idpersona, nombre, apellido, Ndoc, TipoDoc, Ocupacion, FechaNac, nacionalidad) values (id5, 'Renell', 'St. Quentin', '6694351917', 'DNI', 'Nuclear Power Engineer', '2021-01-19', 7);
-insert into pasajero (idpersona, nombre, apellido, Ndoc, TipoDoc, Ocupacion, FechaNac, nacionalidad) values (id6, 'Colene', 'Rogan', '3828106110', 'Otro', 'Assistant Professor', '2021-02-14', 7);
-insert into pasajero (idpersona, nombre, apellido, Ndoc, TipoDoc, Ocupacion, FechaNac, nacionalidad) values (id7, 'Richy', 'Wieprecht', '3533356477', 'LE', 'Geological Engineer', '2021-06-25', 5);
-insert into pasajero (idpersona, nombre, apellido, Ndoc, TipoDoc, Ocupacion, FechaNac, nacionalidad) values (id8, 'Ewan', 'Latek', '0084924403', 'LC', 'Office Assistant I', '2021-04-03', 5);
-insert into pasajero (idpersona, nombre, apellido, Ndoc, TipoDoc, Ocupacion, FechaNac, nacionalidad) values (id9, 'Shandee', 'Record', '3017832678', 'DNI', 'Biostatistician III', '2021-02-23', 8);
-insert into pasajero (idpersona, nombre, apellido, Ndoc, TipoDoc, Ocupacion, FechaNac, nacionalidad) values (id10, 'Artus', 'Munn', '2688732048', 'DNI', 'Clinical Specialist', '2021-09-20', 7);
-
---Datos TipoHabitacion
-insert into tipohabitacion (id, tipo, costo) values (1, 'Simple', 99.99);
-insert into tipohabitacion (id, tipo, costo) values (2, 'Doble', 199.99);
-
---Datos Habitacion
-insert into habitacion (numero, piso, tipo, capacidad, descuento) values ( 1, 1, 1, 1, 0);
-insert into habitacion (numero, piso, tipo, capacidad, descuento) values ( 2, 1, 2, 2, 0);
-insert into habitacion (numero, piso, tipo, capacidad, descuento) values ( 3, 1, 1, 1, 0);
-insert into habitacion (numero, piso, tipo, capacidad, descuento) values ( 1, 2, 1, 1, 0);
-insert into habitacion (numero, piso, tipo, capacidad, descuento) values ( 2, 2, 2, 2, 0);
-insert into habitacion (numero, piso, tipo, capacidad, descuento) values ( 3, 2, 1, 2, 0);
+insert into pasajero (idpersona, nombre, apellido, Ndoc, TipoDoc, Ocupacion, FechaNac, nacionalidad) values (id1, 'Bondy', 'Rylatt', '3808721502', 'LC', 'Office Assistant IV', '2021-09-11', 054);
+insert into pasajero (idpersona, nombre, apellido, Ndoc, TipoDoc, Ocupacion, FechaNac, nacionalidad) values (id2, 'Luce', 'Theml', '9936502027', 'LE', 'Chief Design Engineer', '2021-02-13', 056);
+insert into pasajero (idpersona, nombre, apellido, Ndoc, TipoDoc, Ocupacion, FechaNac, nacionalidad) values (id3, 'Ricki', 'Lapworth', '7519091015', 'Otro', 'Food Chemist', '2021-06-21', 054);
+insert into pasajero (idpersona, nombre, apellido, Ndoc, TipoDoc, Ocupacion, FechaNac, nacionalidad) values (id4, 'Franny', 'Gumme', '9093832379', 'LC', 'Junior Executive', '2021-08-10', 055);
+insert into pasajero (idpersona, nombre, apellido, Ndoc, TipoDoc, Ocupacion, FechaNac, nacionalidad) values (id5, 'Renell', 'St. Quentin', '6694351917', 'DNI', 'Nuclear Power Engineer', '2021-01-19', 057);
+insert into pasajero (idpersona, nombre, apellido, Ndoc, TipoDoc, Ocupacion, FechaNac, nacionalidad) values (id6, 'Colene', 'Rogan', '3828106110', 'Otro', 'Assistant Professor', '2021-02-14', 057);
+insert into pasajero (idpersona, nombre, apellido, Ndoc, TipoDoc, Ocupacion, FechaNac, nacionalidad) values (id7, 'Richy', 'Wieprecht', '3533356477', 'LE', 'Geological Engineer', '2021-06-25', 055);
+insert into pasajero (idpersona, nombre, apellido, Ndoc, TipoDoc, Ocupacion, FechaNac, nacionalidad) values (id8, 'Ewan', 'Latek', '0084924403', 'LC', 'Office Assistant I', '2021-04-03', 055);
+insert into pasajero (idpersona, nombre, apellido, Ndoc, TipoDoc, Ocupacion, FechaNac, nacionalidad) values (id9, 'Shandee', 'Record', '3017832678', 'DNI', 'Biostatistician III', '2021-02-23', 058);
+insert into pasajero (idpersona, nombre, apellido, Ndoc, TipoDoc, Ocupacion, FechaNac, nacionalidad) values (id10, 'Artus', 'Munn', '2688732048', 'DNI', 'Clinical Specialist', '2021-09-20', 057);
 
 --Datos Reserva
 insert into reserva (id_reserva, numero, piso, nombre, apellido, telefono, fechadesde, fechahasta) values (1, 1, 1, 'Bondy', 'Rylatt', '3808721502', '2021-09-11', '2021-09-20');
 insert into reserva (id_reserva, numero, piso, nombre, apellido, telefono, fechadesde, fechahasta) values (2, 1, 1, 'Franny', 'Gumme', '9093832379', '2021-09-21', '2021-09-30');
 insert into reserva (id_reserva, numero, piso, nombre, apellido, telefono, fechadesde, fechahasta) values (3, 1, 1, 'Richy', 'Wieprecht', '3533356477', '2021-09-01', '2021-09-10');
-
---Datos Ocupacion
-insert into ocupacion (id_ocupacion, numero, piso, responsable, checkin, checkout) values (1, 1, 2, id1, '2021-09-21', '2021-09-30');
-insert into ocupacion (id_ocupacion, numero, piso, responsable, checkin, checkout) values (2, 1, 2, id2, '2021-09-01', '2021-09-12');
-insert into ocupacion (id_ocupacion, numero, piso, responsable, checkin, checkout) values (3, 1, 2, id3, '2021-09-13', '2021-09-15');
 
 --Datos Fuera De Servicio
 insert into fueradeservicio (id_fueradeservicio, numero, piso, desde, hasta) values (1, 2, 1,'2021-09-01', '2021-09-30');
